0d84d99e5cc4ab28b9dfeaec1b848eb9ac8d2310
Ryflin <rydogger@gmail.com>
2024-08-15 10:11:33 -0400

    added a player and rendering feature

--- player.go
+++ player.go
@@ -8,13 +8,30 @@ type Player struct {
 }
 
 // move the player around
-func (this *Player) move(maze [][]string, dir int) {
+func (this *Player) move(maze [][]string, dir int) (newMaze [][]string) {
 	newX := this.X + dirs[dir][0]
 	newY := this.Y + dirs[dir][1]
 	if maze[newX][newY] != wallRep {
-		maze[this.X][this.Y] = sqrRep
+		mazeMutex.Lock()
+		maze[this.X][this.Y] = emptyTile
 		this.X = newX + 1
 		this.Y = newY + 1
 		maze[this.X][this.Y] = this.Symbol
+		mazeMutex.Unlock()
 	}
+	return maze
+}
+
+func (this *Player) placePlayer(maze [][]string) (newMaze [][]string) {
+	for maze[this.X][this.Y] != emptyTile {
+		if maze[this.X][this.Y+1] == emptyTile {
+			this.Y += 1
+		} else {
+			this.X += 1
+		}
+	}
+	mazeMutex.Lock()
+	maze[this.X][this.Y] = this.Symbol
+	mazeMutex.Unlock()
+	return maze
 }
--- render.go
+++ render.go
@@ -1,5 +1,26 @@
 package main
 
-func render(maze [][]string,) {
-	
+import (
+	"fmt"
+	"time"
+)
+
+func render(maze [][]string) {
+	// TODO figure out a way to clear the command line
+	// syscall.Exec("clear")
+	// for i := 0; i < 1; i++ {
+
+	// }
+	for _, row := range maze {
+		for _, val := range row {
+			fmt.Print(val, " ")
+		}
+		fmt.Println()
+	}
+
+	for i := 0; i < len(maze); i++ {
+		fmt.Print(escUp)
+		nullable = i
+	}
+	time.Sleep(time.Second)
 }
